5bdbf4f98a29aaf6c3e4e88197d5a94ff380de94
adam grocholski <adgroc@outlook.com>
2015-06-30 13:26:03 -0500

    add tokens controller

---
+++ src/Trollbridge.WebApi/Controllers/TokensController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Trollbridge.Core;
+
+namespace Trollbridge.WebApi.Controllers
+{
+    public class TokensController : ApiController
+    {
+        public TokenHolder Get(string id)
+        {
+            return SasTokenHelper.GetTokensUncompressed(id);
+        }
+
+
+        public TokenHolder Get(string id, bool compress)
+        {
+            if (compress)
+                return SasTokenHelper.GetTokensCompressed(id);
+            else
+                return SasTokenHelper.GetTokensUncompressed(id);
+        }
+    }
+}
