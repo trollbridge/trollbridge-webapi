22c2fec0dbb32bc01240825e81255dad109fa821
adam grocholski <adgroc@outlook.com>
2015-06-30 14:18:55 -0500

    added documentation to api

--- src/Trollbridge.WebApi/Controllers/TokensController.cs
+++ src/Trollbridge.WebApi/Controllers/TokensController.cs
@@ -10,12 +10,22 @@ namespace Trollbridge.WebApi.Controllers
 {
     public class TokensController : ApiController
     {
+        /// <summary>
+        /// Gets SAS tokens to enable a device to access Azure blobs, queues, and event hubs.
+        /// </summary>
+        /// <param name="id">Id of device to return SAS tokens for.</param>
+        /// <returns></returns>
         public TokenHolder Get(string id)
         {
             return SasTokenHelper.GetTokensUncompressed(id);
         }
 
-
+        /// <summary>
+        /// Gets SAS tokens to enable a device to access Azure blobs, queues, and event hubs.
+        /// </summary>
+        /// <param name="id">Id of device to return SAS tokens for.</param>
+        /// <param name="compress">Whether or not compress the SAS tokens.</param>
+        /// <returns></returns>
         public TokenHolder Get(string id, bool compress)
         {
             if (compress)
